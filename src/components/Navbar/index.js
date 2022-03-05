cbedda1e0239a8066e034e6907e7cc558e099f78
Mateus-Arauj <mateus9509195091@gmail.com>
2022-03-05 01:44:16 -0300

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,64 @@
+import React,{useState} from 'react';
+import './Navbar.css'
+import logo from '../../img/logo.png'
+import {Link} from 'react-router-dom'
+
+
+// Icons Material UI
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
+const Navbar = ()=>{
+    const [click, setClick] = useState(false)
+    const Mouseon = (e) =>{
+        e.style.fontSize = "35px"
+    }
+    const Mouseout = (e)=>{
+        e.style.fontSize = "30px"
+    }
+    const handleClick = ()=>{
+        setClick(state => !state)
+    }
+    return (
+        <nav className = "navbar">
+            <Link to="/" className = "logo-nav">
+                <img src={logo} alt="logo"/>
+            </Link>
+            <div className = "menu-icon" onClick = {handleClick}>
+                {click ? <CloseIcon style={{color: "#fff", fontSize: "40px"}}/> : <MenuIcon style={{color: "#fff", fontSize: "40px"}}/>}
+            </div>
+            <ul className = {click? "nav-menu active" : "nav-menu"}>
+                <li className="nav-item">
+                    <a>Home</a>
+                </li>
+                <li className="nav-item">
+                <a>Services</a>
+                </li>
+                <li className="nav-item">
+                    <a>About</a>
+                </li>
+                <li className="nav-item">
+                    <a>Contact</a>
+                </li>
+
+            </ul>
+            <ul className = "social-media">
+                <li>
+                    <a className="instagram" href = "https://www.instagram.com/_mateusaraujoo_/" target="_blank" onMouseOver={(e)=>{Mouseon(e.target)}} onMouseOut = {(e)=>{Mouseout(e.target)}}>
+                       <InstagramIcon style={{color: "rgb(220,220,220)", fontSize: "30px"}} />
+                    </a>
+                </li>
+                <li>
+                    <a className = "github" href = "https://github.com/Mateus-Arauj" target="_blank" onMouseOver={(e)=>{Mouseon(e.target)}} onMouseOut = {(e)=>{Mouseout(e.target)}}>
+                        <GitHubIcon style={{color: "rgb(220,220,220)", fontSize: "30px"}} />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+
+export default Navbar
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+
+const HomePage = ()=>{
+
+    return (
+        <div>
+            <Navbar/>
+        </div>
+        
+    )
+}
+
+export default HomePage
